97ee805d7cb45910978c80674dfa8b5231fd98b1
Joseph Rollins <rollins.joseph@gmail.com>
2016-02-25 21:06:48 -0500

    Connect to AMQP and add test package publishing every 2 sec

--- apps/watch/lib/watch/registry.ex
+++ apps/watch/lib/watch/registry.ex
@@ -2,15 +2,16 @@ defmodule Watch.Registry do
   use GenServer
   require Logger
 
+  alias Timex.{Date,DateFormat}
+
   @npm_url "https://skimdb.npmjs.com/registry/_design/app/_view/updated"
 
   defp update_url(key) do
     @npm_url <> "?" <> URI.encode_query(%{start_key: "\"#{key}\"", limit: 5})
   end
 
-  defp report_package(%{"id" => name, "key" => key}) do
+  defp publish_package(%{"id" => name, "key" => key}, chan) do
     Logger.info "Package: #{name}"
-    key
   end
 
   defp poll(key) do
@@ -19,11 +20,10 @@ defmodule Watch.Registry do
     |> Poison.decode!
     |> Map.get("rows")
     |> Enum.drop_while(&(Map.get(&1, "key") == key))
-    |> Enum.map(&report_package/1)
   end
 
-  defp send_after do
-    Process.send_after(self(), :poll, 2 * 1000)
+  defp send_after(msg) do
+    Process.send_after(self(), msg, 2 * 1000)
   end
 
   # Client
@@ -33,22 +33,38 @@ defmodule Watch.Registry do
     GenServer.start_link(__MODULE__, nil)
   end
 
-  def init(args) do
-    send_after()
-    Timex.Date.now |> Timex.DateFormat.format("{ISOz}")
+  def init(_args) do
+    send_after(:poll)
+    send_after(:test)
+
+    {:ok, connection} = AMQP.Connection.open
+    {:ok, channel} = AMQP.Channel.open(connection)
+    AMQP.Queue.declare(channel, "npm")
+
+    {:ok, key} = Date.now |> DateFormat.format("{ISOz}")
+    {:ok, {key, channel}}
   end
 
   # Callbacks
 
-  def handle_info(:poll, key) do
-    key = case poll(key) do
-      [] ->
-        key
-      keys ->
-        Enum.at(keys, -1)
+  def handle_info(:test, {key, chan}) do
+    publish_package(%{
+      "id" => "test_please_ignore",
+      "key" => DateFormat.format(Date.now, "{ISOz}")
+    }, chan)
+    send_after(:test)
+    {:noreply, {key, chan}}
+  end
+
+  def handle_info(:poll, {key, chan}) do
+    packages = poll(key)
+    Enum.map(packages, &(publish_package(&1, chan)))
+    send_after(:poll)
+    next_key = case packages do
+                 [] -> key
+                 [%{"key" => key}|_tail] -> key
     end
-    send_after()
-    {:noreply, key}
+    {:noreply, {next_key, chan}}
   end
 
   def handle_info(msg, state) do
